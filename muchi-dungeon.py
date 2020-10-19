921d888ac13897c58b6eb892053f4b633af0fe3c
Takuya Oyaidu <gachianiki69@gmail.com>
2020-10-20 02:02:19 +0900

    PORTの変更

--- muchi-dungeon.py
+++ muchi-dungeon.py
@@ -1,4 +1,5 @@
 import connexion
+import os
 
 
 app = connexion.FlaskApp(__name__)
@@ -6,4 +7,4 @@ app = connexion.FlaskApp(__name__)
 
 if __name__ == '__main__':
     app.add_api('muchi-dungeon.yaml')
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
