8106e4a44b2dfe33f37f6203563a8232f0c3c7a7
Takuya Oyaidu <gachianiki69@gmail.com>
2020-10-20 01:39:07 +0900

    初回コミット(空HTML)

---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn muchi-dungeon:app --log-file -
---
+++ muchi-dungeon.py
@@ -0,0 +1,9 @@
+import connexion
+
+
+app = connexion.FlaskApp(__name__)
+
+
+if __name__ == '__main__':
+    app.add_api('muchi-dungeon.yaml')
+    app.run(host='0.0.0.0', port=5000, debug=True)
